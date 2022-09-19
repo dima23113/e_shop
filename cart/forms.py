f1d07375eee71139f1156a6df66869e4153ae748
dima23113 <namedima4@gmail.com>
2022-09-20 00:47:40 +0300

    Добавил вывод адреса в форму заказа. Если адреса нет, то ссылку на добавление

--- cart/forms.py
+++ cart/forms.py
@@ -3,6 +3,5 @@ from django import forms
 
 class DeliveryForm(forms.Form):
     delivery = [('pvz', 'Пункт выдачи'), ('cureer', 'Курьером')]
-    city = forms.CharField(required=True, initial=False, )
-    zipcode = forms.CharField(required=True, initial=False)
+    address = forms.ChoiceField(required=True, initial=False)
     delivery_type = forms.ChoiceField(choices=delivery, required=True, initial=False)
